5b2ecd276d5d6873ce1d29f0adefb8d44c873f8b
dimasrehannabiel <dimasrehannabiel@gmail.com>
2017-04-06 11:09:35 +0700

    Test file untuk merge

---
+++ index.php
@@ -0,0 +1,17 @@
+<html>
+    <head>
+        <title>Coba-Merge</title>
+    </head>
+    <body>
+        <module-one>
+            <ul>
+                <li>Item Pertama</li>
+                <li>Item Kedua</li>
+                <li>Item Ketiga</li>
+                <li>Item Keempat</li>
+                <li>Item Kelima</li>
+            </ul>
+            
+        </module-one>        
+    </body>
+</html>
