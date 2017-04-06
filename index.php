6a8106314e907dfcd228d943622bc195bdf8cbe2
Ari Setiawan <loominius@gmail.com>
2017-04-06 14:53:24 +0700

    han rehann :v

--- index.php
+++ index.php
@@ -1,11 +1,6 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="utf-8">
-    <title></title>
-  </head>
-  <body>
-    <h1>INI BUAT NYOBA TEAM WORK DENGAN GIT</h1>
-    <p>Gaboleh niru ya.. dosa lo</p>
-  </body>
-</html>
+<?php
+
+  echo "this file HAS BEEN HACKED by rayholic";
+  echo "rehan";
+
+?>
