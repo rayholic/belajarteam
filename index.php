1848aeeb031642df0181e59d501f4cf70fe66d7a
Ari Setiawan <loominius@gmail.com>
2017-04-06 11:15:19 +0700

    ini percobaan

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <h1>INI BUAT NYOBA TEAM WORK DENGAN GIT</h1>
+    <p>Gaboleh niru ya.. dosa lo</p>
+  </body>
+</html>
