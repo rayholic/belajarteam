1cddc8b9b99b5a050cf1f13aa1433ecd7635ab5c
dimasrehannabiel <dimasrehannabiel@gmail.com>
2017-04-06 15:28:00 +0700

    Edited by DimasRehanNabiel

--- index.php
+++ index.php
@@ -7,5 +7,8 @@
   <body>
     <h1>INI BUAT NYOBA TEAM WORK DENGAN GIT</h1>
     <p>Gaboleh niru ya.. dosa lo</p>
+      
+    <h2 style="color:red">SUDAH TERCURRY WOI AKWOKWAOWA</h2>  
+    <p>T E R C U R R Y</p>  
   </body>
 </html>
